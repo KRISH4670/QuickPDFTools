9866eb5ba51415fc473ebf7aa4488b1bed2d5bcb
KRISH4670 <krishmeena0549@gmail.com>
2026-09-22 11:08:32 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('[data-menu-toggle]');
-  const menu = document.querySelector('[data-mobile-menu]');
-  if (button && menu) {
-    button.addEventListener('click', () => {
-      menu.classList.toggle('open');
-    });
-  }
-});
